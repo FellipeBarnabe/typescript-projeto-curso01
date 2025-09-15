3b3ed74461c2755c04783517ed6ffb3c0b2fc1c6
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2025-09-15 19:12:41 -0300

    tsFile

--- dist/js/bytebank.js
+++ dist/js/bytebank.js
@@ -1,32 +1,47 @@
-import { TipoTransacao } from "./types/TipoTransacao";
-
+import { TipoTransacao } from "../dist/js/types/TipoTransacao";
 let saldo = 3000;
-
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
-elementoSaldo.textContent = saldo;
-
+if (elementoSaldo !== null) {
+    elementoSaldo.textContent = saldo.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    });
+}
 const elementoFormulario = document.querySelector(".block-nova-transacao form");
 elementoFormulario.addEventListener("submit", function (event) {
-  event.preventDefault();
-  if (!elementoFormulario.checkVisibility()) {
-    alert("Por favor, preencha todos os campos da transação!");
-    return;
-  }
-
-  const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
-  const inputValor = elementoFormulario.querySelector("#valor");
-  const inputData = elementoFormulario.querySelector("#data");
-
-  let tipoTransacao = inputTipoTransacao.value;
-  let valor = inputValor.value;
-  let data = inputData.value;
-
-  const novaTransacao = {
-    TipoTransacao: TipoTransacao,
-    valor: valor,
-    data: data,
-  };
-
-  console.log(novaTransacao);
-  elementoFormulario.reset();
+    event.preventDefault();
+    if (!elementoFormulario.checkVisibility()) {
+        alert("Por favor, preencha todos os campos da transação!");
+        return;
+    }
+    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    const inputValor = elementoFormulario.querySelector("#valor");
+    const inputData = elementoFormulario.querySelector("#data");
+    let tipoTransacao = inputTipoTransacao.value;
+    let valor = inputValor.valueAsNumber;
+    let data = new Date(inputData.value);
+    if (tipoTransacao == "deposito") {
+        saldo += valor;
+    }
+    else if (tipoTransacao == "transferencia" ||
+        tipoTransacao == "pagamento de boleto") {
+        saldo -= valor;
+    }
+    else {
+        alert("Não foi possivel realizar a transação");
+        return;
+    }
+    if (elementoSaldo !== null) {
+        elementoSaldo.textContent = saldo.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+        });
+    }
+    const novaTransacao = {
+        TipoTransacao: TipoTransacao,
+        valor: valor,
+        data: data,
+    };
+    console.log(novaTransacao);
+    elementoFormulario.reset();
 });
